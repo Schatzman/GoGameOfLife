8dee660caf8bdeae3e236bb89f86b5ba990a099a
Schatzman <djschatzmiller@gmail.com>
2017-10-20 08:57:51 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [ -d $GOSRCPATH ]; then
-    cp -r GameOfLife $GOSRCPATH/src/
+    cp -r GameOfLife $GOSRCPATH/
 else
     echo "Please set "'$GOSRCPATH' should be `$GOPATH/src`" - https://github.com/golang/go/wiki/Setting-GOPATH"
 fi
 
 go install GameOfLife
-# for tomorrow: error with info about setting up $GOSRCPATH if above fails
